a9d8fa224124df0756eab06c85486c0c4730d318
Rosario Nici <rosarionici95@gmail.com>
2022-10-06 13:56:10 +0200

    Installing Bootstrap ad fontawesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
+
+
+
+/* Set up using Vue 2 */
+import Vue from 'vue'
+import App from './App'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
